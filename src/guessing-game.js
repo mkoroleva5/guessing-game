d7b48ded6df678d05fcd6daeee0cc37200e33efa
Koroleva Maria <mkoroleva5@gmail.com>
2022-08-14 14:26:16 +0300

    feat: implement solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.number = Math.round((this.min + this.max) / 2);
+        return this.number;
     }
 
     lower() {
-
+        this.max = this.number;
     }
 
     greater() {
-
+        this.min = this.number;
     }
 }
 
